b77b91846af6aa17b8feed13d631b2983258bd05
mrisholukamba <abdulrazzaqlukamba@gmail.com>
2023-05-26 22:41:43 +0300

    Genesis Demo

---
+++ src/app/Context/store.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { createContext, useContext, Dispatch, SetStateAction, useState } from "react";
+
+// Wallet Context
+
+
+// Payment Progress
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,143 @@
+'use client';
+
 import Image from 'next/image'
+import Link from 'next/link'
+// Material UI
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen w-full flex-col items-center justify-between p-2">
+      {/*Welcoming Witty text */}
+      <div className="w-4/6  h-25 max-h-50 p-5 items-center  text-white  bg-gray-800 flex flex-col justify-around">
+         <h1 className="font-semibold text-lg w-4/6"><PermIdentityIcon/><Link href="/profile">GM User</Link>  <span className="font-semibold mx-9 italic">What do you want to Experience today?</span></h1>
+         {/* tl;dr Activity, Analytics, Plan */}
+         <div className="flex justify-around align-middle bg-transparent h-24 p-5 w-full">
+            <div>Configuration<h1 className="font-semibold underline underline-offset-8">Wallet</h1></div>
+            <div >Ads Analytics<h1 className="font-semibold underline underline-offset-8">100 impressions</h1></div>
+            <div>Subscription Plan<h1 className="font-semibold underline underline-offset-8">Bronze</h1></div>
+            <div>Clients Community Members(DAO)<h1 className="font-semibold underline underline-offset-8">5</h1></div>
+         </div>
+      </div>
+
+      {/* Products*/ }
+      <div className="w-3/5 h-130 max-h-150 p-5 flex flex-wrap justify-evenly flex-row bg-white">
+        <div className="m-5">
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                  Secure & Risk-Free Payment
+              </Typography>
+              <Typography variant="body2">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small"><Link href="/payment">Send</Link></Button>
+            </CardActions>
+          </Card>
+        </div>
+
+        <div className="m-5">
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                  Run On-Chain Ads
+              </Typography>
+              <Typography variant="body2">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small"><Link href="/aoc">Learn More</Link></Button>
+            </CardActions>
+          </Card>
+        </div>
+ 
+        <div className="m-5">
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                Build Your Client Community.
+              </Typography>
+              <Typography variant="body2">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small"><Link href="/dao">Learn More</Link></Button>
+            </CardActions>
+          </Card>
+        </div>
+
+        <div className="m-5">
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                  Store & Grow your Capital
+              </Typography>
+              <Typography variant="body2">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small"><Link href="/dex">Learn More</Link></Button>
+            </CardActions>
+          </Card>
+        </div>
+
+        <div className="m-5">
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                  Use WhatsApp Interface
+              </Typography>
+              <Typography variant="body2">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small"><Link href="/web2">Learn More</Link></Button>
+            </CardActions>
+          </Card>
         </div>
       </div>
+      
+      {/* Setting up profile & Wallet*/}
+      <div className="w-1/2 h-20 max-h-50 align-middle flex  justify-around p-4 flex-row bg-slate-50">
+        <div>
+          <AccountBalanceWalletIcon/>
+        <Button size="small"><Link href="/wallet">Configure Wallet</Link></Button>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div>
+          <SettingsApplicationsIcon/>
+        <Button size="small"><Link href="/setting">Settings</Link></Button>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div>
+          <CurrencyBitcoinIcon/>
+        <Button size="small"><Link href="/plan">Choose Plan</Link></Button>
+        </div>
       </div>
     </main>
   )
