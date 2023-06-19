de7f162b32c2163dbe31c6f55c74d688d628f497
mrisholukamba <abdulrazzaqlukamba@gmail.com>
2023-06-20 00:15:38 +0300

    UI SM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
          <h1 className="font-semibold p-2 flex flex-col sm:flex-row align-middle justify-center sm:mx-4 h-auto text-sm w-5/6"><PermIdentityIcon/><Link href="/profile">GM {account? account.meta.name : 'Anon'}</Link> </h1>
          {/* tl;dr Activity, Analytics, Plan */}
          <Card elevation={0} className="text-xs w-full sm:text-base mt-5 mb-1" sx={{display:"flex",backgroundColor:"inherit",flexDirection:"row",justifyContent:"space-around",flexWrap:"wrap"}}>
-            <Card elevation={0} sx={{display:"flex",backgroundColor:"inherit", alignItems:"center",flexDirection:"column"}}>Config<h1 className="font-medium text-sm ">Wallet</h1></Card>
+            <Card className="hidden sm:flex sm:flex-col" elevation={0} sx={{display:"flex",backgroundColor:"inherit", alignItems:"center"}}>Config<h1 className="font-medium text-sm ">Wallet</h1></Card>
             <Card elevation={0} sx={{display:"flex",backgroundColor:"inherit", alignItems:"center",flexDirection:"column"}}>Ads-Analytics<h1 className="font-medium text-sm ">100 impressions</h1></Card>
             <Card elevation={0} sx={{display:"flex",backgroundColor:"inherit", alignItems:"center",flexDirection:"column"}}>Plan<h1 className="font-medium text-sm ">Bronze</h1></Card>
             <Card elevation={0} sx={{display:"flex",backgroundColor:"inherit", alignItems:"center",flexDirection:"column"}}>Clients<h1 className="font-medium text-sm ">5</h1></Card>
@@ -46,7 +46,7 @@ export default function Home() {
       
        {/* Setting up profile & Wallet*/}
        <Card variant='outlined' className=" w-full sm:w-4/6 h-auto align-middle flex justify-around p-1 flex-row m-5">
-        <Card elevation={0} className="text-xs h-auto w-20">
+        <Card elevation={0} className=" hidden sm:flex text-xs h-auto w-20">
           <AccountBalanceWalletIcon className="text-xs sm:text-sm"/>
           <Button className="text-xs" ><Link href="/wallet">Wallet</Link></Button>
         </Card>
